22d5d2dbab441192342d4598f6bf052f353994fc
hardsudo <85029764+hardsudo@users.noreply.github.com>
2024-01-13 12:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from transformers import pipeline
+
+# Allocate a pipeline for sentiment-analysis
+nlp = pipeline('sentiment-analysis')
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/sentiment', methods=['POST'])
+def sentiment():
+    # Get the text from the POST request
+    text = request.json.get('text', 'I love transformers!')
+
+    # Perform sentiment analysis
+    result = nlp(text)
+
+    # Return the result as JSON
+    return {'result': result}
